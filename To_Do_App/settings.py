b57fb0e27007acdd9370a683a9881baae42990d8
jatin singh <jatinsigh1810@gmail.com>
2021-02-06 15:24:27 +0530

    Change settings

--- To_Do_App/settings.py
+++ To_Do_App/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY_1')
+SECRET_KEY = '6701b3d673a686e0e8ed352e7886ee44925d525c444d7f99'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
